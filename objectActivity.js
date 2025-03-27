6b021f9391e89bf9e71a295bfcb2cece5f502c5e
MACABENTA, VHARDEY JOHN F. <johnfayusal@gmail.com>
2025-03-28 01:29:03 +0800

    Add files via upload

---
+++ objectActivity.js
@@ -0,0 +1,18 @@
+// MACABENTA, VHARDEY JOHN F.
+let car  = { // creates an object 
+  type : "Sedan",
+  model : "Camry",
+  color : "Blue"
+};
+
+console.log(typeof car) // displays what type of object our car object is
+
+car.type = "Toyota"; // change the value of our car type
+console.log(car); // logs our object 
+
+car.Wheels = 4; // adds new value into our object
+console.log(car); // logs our car object.
+
+/* References:
+https://www.w3schools.com/js/js_objects.asp
+*/
